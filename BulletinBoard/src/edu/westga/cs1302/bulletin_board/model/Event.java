38dd0e9f6d6bc758b19e1838ca5e0a51d6e35572
csuser@cs.westga.edu <csuser@cs.westga.edu>
2022-11-14 15:08:13 -0500

    Feature A Storing, Sorting and Filtering Postings
    1.) created Event class in order to store and Event object with Title, description,Date and Type
    2.) Created precondition checks
    3.) created toString method to return the value of the title of the event

---
+++ BulletinBoard/src/edu/westga/cs1302/bulletin_board/model/Event.java
@@ -0,0 +1,99 @@
+package edu.westga.cs1302.bulletin_board.model;
+
+import java.time.LocalDate;
+
+/**
+ * The Class Event.
+ * 
+ * @author Jeffrey Gaines
+ */
+public class Event {
+
+	private String title;
+	private String description;
+	private LocalDate date;
+	private TypeOfEvent type;
+
+	private enum TypeOfEvent {
+		Musical, Theatrical, Political;
+	}
+
+	/**
+	 * Instantiates a new event.
+	 *
+	 * @param title       the title
+	 * @param description the description
+	 * @param date        the date
+	 * @param type        the type
+	 */
+	public Event(String title, String description, LocalDate date, TypeOfEvent type) {
+		if (title == null) {
+			throw new IllegalArgumentException("Title must not be null");
+		}
+		if (description == null) {
+			throw new IllegalArgumentException("Description must not be null");
+		}
+		if (title.isEmpty()) {
+			throw new IllegalArgumentException("Title must not be empty");
+		}
+		if (description.isEmpty()) {
+			throw new IllegalArgumentException("Description must not be empty");
+		}
+		if (!(type.equals(TypeOfEvent.Musical) || type.equals(TypeOfEvent.Theatrical)
+				|| type.equals(TypeOfEvent.Political))) {
+			throw new IllegalArgumentException("Type must be Musical, Theatrical or Political");
+		}
+
+		if (date != null) {
+			throw new IllegalArgumentException("Date must not be null");
+		}
+
+		this.title = title;
+		this.description = description;
+		this.date = date;
+		this.type = type;
+
+	}
+
+	@Override
+	public String toString() {
+		return "Event [title=" + this.title + "]";
+	}
+
+	/**
+	 * Gets the title.
+	 *
+	 * @return the title
+	 */
+	public String getTitle() {
+		return this.title;
+	}
+
+	/**
+	 * Gets the description.
+	 *
+	 * @return the description
+	 */
+	public String getDescription() {
+		return this.description;
+	}
+
+	/**
+	 * Gets the date.
+	 *
+	 * @return the date
+	 */
+	public LocalDate getDate() {
+		return this.date;
+	}
+
+	/**
+	 * Gets the type.
+	 *
+	 * @return the type
+	 */
+	public TypeOfEvent getType() {
+		return this.type;
+	}
+	
+}
