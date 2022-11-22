68e5974c9a54ce75d7d0da2a6ee252bf317065f1
Jeffrey Gaines <jg00242@my.westga.edu>
2022-11-21 23:24:21 -0500

    Implemented GUI
    1.) Implemented GUI to allow users to input events and add/remove from event list
    2.) Also added sorting comboboxs to allow user to sort between different orders and filters in event list

--- BulletinBoard/src/edu/westga/cs1302/bulletin_board/view/MainWindow.java
+++ BulletinBoard/src/edu/westga/cs1302/bulletin_board/view/MainWindow.java
@@ -1,10 +1,42 @@
 package edu.westga.cs1302.bulletin_board.view;
 
-/** Codebehind for the MainWindow of the Application.
- * 
- * @author CS 1302
- * @version Fall 2022
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+/**
+ * The Class MainWindow.
+ * @author Jeffrey Gaines
  */
 public class MainWindow {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private DatePicker eventDatePicker;
+
+    @FXML
+    private TextField eventDescriptionTextField;
+
+    @FXML
+    private TextField eventTitleTextField;
+
+    @FXML
+    private ComboBox<?> eventTypeComboBox;
+
+    @FXML
+    private ComboBox<?> listOrderComboBox;
+
+    @FXML
+    private ComboBox<?> listTypeComboBox;
+
+    
 
 }
